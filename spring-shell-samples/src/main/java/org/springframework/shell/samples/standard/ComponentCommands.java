28e5d29147087d990f94258e9874eff4872ae0a3
Nicola Di Falco <nicola.difalco@mythalesgroup.io>
2023-08-09 07:10:06 +0200

    fix: display required only if no default value

--- spring-shell-samples/src/main/java/org/springframework/shell/samples/standard/ComponentCommands.java
+++ spring-shell-samples/src/main/java/org/springframework/shell/samples/standard/ComponentCommands.java
@@ -39,14 +39,15 @@ import org.springframework.shell.component.support.SelectorItem;
 import org.springframework.shell.standard.AbstractShellComponent;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 import org.springframework.util.StringUtils;
 
 @ShellComponent
 public class ComponentCommands extends AbstractShellComponent {
 
 	@ShellMethod(key = "component string", value = "String input", group = "Components")
-	public String stringInput(boolean mask, boolean required) {
-		StringInput component = new StringInput(getTerminal(), "Enter value", null);
+	public String stringInput(boolean mask, boolean required, @ShellOption(defaultValue = ShellOption.NULL) String defaultValue) {
+		StringInput component = new StringInput(getTerminal(), "Enter value", defaultValue);
 		component.setResourceLoader(getResourceLoader());
 		component.setTemplateExecutor(getTemplateExecutor());
 		component.setRequired(required);
